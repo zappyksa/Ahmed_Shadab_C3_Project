5ec3d26612d12c10276d0e9fa9232a212c894d4f
zappyksa <zappy.ksa1@gmial.com>
2022-09-19 22:37:27 +0300

    Part 3: Failing test case

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.LocalTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -72,6 +73,17 @@ class RestaurantTest {
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>GetCost<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    @Test
+    public void select_item_from_list_should_return_order_cost(){
+        int totalCost;
+        List<Item> selectedItems = null;
+        totalCost = restaurant.getTotalCost(selectedItems) ;
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<GetCost>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 }
 
